d3e922d214d8906fd3d19269acc4619f74cec480
forrestmorrison <forrestmorrison@gmail.com>
2023-07-04 20:25:10 -0500

    redux store & provider

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,8 @@
 import { Routes, Route, useNavigate } from "react-router-dom"
 import { createTheme } from '@mui/material/styles'
 import { ThemeProvider } from '@emotion/react';
+import { Provider } from "react-redux";
+import { store } from "./app/store";
 import Home from "./pages/Home"
 import Nav from "./components/Nav";
 import Login from "./auth/Login";
@@ -25,20 +27,22 @@ const theme = createTheme({
 
 function App() {
   return (
-    <ThemeProvider theme={theme}>
-      <div className="App">
-        <header>
-          <Nav />
-        </header>
-        <main>
-          <Routes>
-            <Route exact path="/" element={<Home />} />
-            <Route exact path="/login" element={<Login />} />
-            <Route exact path="/signup" element={<Signup />} />
-          </Routes>
-        </main>
-      </div>
-    </ThemeProvider>
+    <Provider store={store}>
+      <ThemeProvider theme={theme}>
+        <div className="App">
+          <header>
+            <Nav />
+          </header>
+          <main>
+            <Routes>
+              <Route exact path="/" element={<Home />} />
+              <Route exact path="/login" element={<Login />} />
+              <Route exact path="/signup" element={<Signup />} />
+            </Routes>
+          </main>
+        </div>
+      </ThemeProvider>
+    </Provider>
   );
 }
 
