f1de0eccc61d61aa9265bc5a7c93d1749bfda85a
forrestmorrison <forrestmorrison@gmail.com>
2023-07-05 18:57:16 -0500

    created BitcoinPrice component for fetching current price from  API

---
+++ client/src/components/BitcoinPrice.js
@@ -0,0 +1,12 @@
+import { FaBitcoin } from "react-icons/fa"
+
+const BitcoinPrice = () => {
+  return (
+    <div className="btc-price">
+        <h4>Current <FaBitcoin size="24px" style={{ margin: "0px 10px", color: "#F2A900"}}/> Price:</h4>
+        <h4>$000</h4>
+    </div>
+  )
+}
+
+export default BitcoinPrice
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,8 +1,10 @@
 import Calculator from "../components/Calculator";
+import BitcoinPrice from "../components/BitcoinPrice";
 
 const Home = () => {
   return (
     <div>
+      <BitcoinPrice />
       <Calculator />
     </div>
   )
