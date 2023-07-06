e70dde6666eccdcd58eacbd5c2b7df666fc301c0
forrestmorrison <forrestmorrison@gmail.com>
2023-07-05 22:45:01 -0500

    added diagram

--- client/src/components/BitcoinPrice.js
+++ client/src/components/BitcoinPrice.js
@@ -3,7 +3,7 @@ import { FaBitcoin } from "react-icons/fa"
 const BitcoinPrice = () => {
   return (
     <div className="btc-price">
-        <h4>Current <FaBitcoin size="24px" style={{ margin: "0px 10px", color: "#F2A900"}}/> Price:</h4>
+        <h4>Current <FaBitcoin size="18px" style={{ margin: "0px 10px", color: "#F2A900"}}/> Price:</h4>
         <h4>$000</h4>
     </div>
   )
