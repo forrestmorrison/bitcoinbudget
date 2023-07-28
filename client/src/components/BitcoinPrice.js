e88017342704f0209da429cc6d5d14e7ec6fd430
forrestmorrison <forrestmorrison@gmail.com>
2023-07-28 14:41:36 -0500

    DISPLAYING CURRENT BTC PRICE NO ERRORS!

--- client/src/components/BitcoinPrice.js
+++ client/src/components/BitcoinPrice.js
@@ -3,13 +3,14 @@ import { FaBitcoin } from "react-icons/fa"
 import axios from "axios"
 
 const BitcoinPrice = () => {
-  const [coins, setCoins] = useState([])
+  const [coin, setCoin] = useState([])
+  const currentPrice = coin[0]
 
   const url = "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&ids=bitcoin&order=market_cap_desc&per_page=1&page=1&sparkline=false&locale=en&precision=2"
 
   useEffect(() => {
     axios.get(url).then((res) => {
-      setCoins(res.data)
+      setCoin(res.data)
       console.group(res.data[0])
     }).catch((error) => {
       console.log(error)
@@ -19,7 +20,7 @@ const BitcoinPrice = () => {
   return (
     <div className="btc-price">
         <h4>Current <FaBitcoin size="18px" style={{ margin: "0px 10px", color: "#F2A900"}}/> Price:</h4>
-        <h4>$000</h4>
+        <h4>{currentPrice.current_price}</h4>
     </div>
   )
 }
