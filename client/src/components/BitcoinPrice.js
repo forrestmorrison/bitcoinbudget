1fc81b73fa722ebb6ab75b1a86722d8e29a8f00c
forrestmorrison <forrestmorrison@gmail.com>
2023-07-28 13:47:42 -0500

    installed Axios, added useState & useEffect to Bitcoin Price

--- client/src/components/BitcoinPrice.js
+++ client/src/components/BitcoinPrice.js
@@ -1,6 +1,21 @@
+import { useState, useEffect } from "react"
 import { FaBitcoin } from "react-icons/fa"
+import axios from "axios"
 
 const BitcoinPrice = () => {
+  const [coins, setCoins] = useState([])
+
+  const url = "/"
+
+  useEffect(() => {
+    axios.get(url).then((res) => {
+      setCoins(res.data)
+      console.group(res.data[0])
+    }).catch((error) => {
+      console.log(error)
+    })
+  }, [])
+
   return (
     <div className="btc-price">
         <h4>Current <FaBitcoin size="18px" style={{ margin: "0px 10px", color: "#F2A900"}}/> Price:</h4>
